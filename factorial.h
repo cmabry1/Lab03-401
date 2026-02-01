89e4edcf8811a93bf94d01cd30a5e74b2281d683
cmabry1 <143213754+cmabry1@users.noreply.github.com>
2026-02-01 11:13:38 -0600

    Add header file for factorial functions

---
+++ factorial.h
@@ -0,0 +1,10 @@
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+#define MAX 100
+
+// Function prototypes
+unsigned long long factorial_iterative(int n);
+unsigned long long factorial_recursive(int n);
+
+#endif
